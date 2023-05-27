cd63924467b9a7145f36b699338ddb3ccd3eb8f9
LeidZhang <haoz28889@gmail.com>
2023-05-27 01:29:53 -0400

    django and mysql

---
+++ backend/camera/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+from .import views 
+
+urlpatterns = [
+    path("", views.camera_list, name="camera_list"), 
+    path("int:id", views.camera_detail, name="camera_detail")
+]
