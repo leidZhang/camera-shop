c2a00c1d01d75bc0d08c8e4a3c003b43a80977f5
LeidZhang <haoz28889@gmail.com>
2023-05-31 13:14:28 -0400

    cart UI

--- src/component/ProductItem/ProductItem.js
+++ src/component/ProductItem/ProductItem.js
@@ -1,5 +1,5 @@
-import { useNavigate } from "react-router-dom";
 import './ProductItem.css'; 
+import { useNavigate } from "react-router-dom";
 
 const ProductItem = (props) => {
   const navigate = useNavigate();
