cd1a478680852e84729a673aae6b7b212a6b1ace
LeidZhang <haoz28889@gmail.com>
2023-06-03 03:00:43 -0400

    cart item deletion

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views 
 
 urlpatterns = [
-    path("<int:id>", views.cart_items_by_id, name="cart_items")
+    path("user=<int:uid>", views.cart_items_by_user_id, name="cart_item_by_user_id"),
+    path("delete/user=<int:user_id>&id=<int:row_id>", views.delete_by_id, name="delete_by_id")
 ]
